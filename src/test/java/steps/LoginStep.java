28d7421b9c7bc8810c312a7038b361f129e377e8
Lukasz <lucas.kaczmarczyk@gmail.com>
2017-11-23 14:39:18 +0100

    Scenario created and LoginStep

---
+++ src/test/java/steps/LoginStep.java
@@ -0,0 +1,28 @@
+package steps;
+
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class LoginStep {
+    @Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+
+    }
+
+    @And("^I enter the username as admin and password as admin$")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() throws Throwable {
+
+    }
+
+    @And("^I click login button$")
+    public void iClickLoginButton() throws Throwable {
+
+    }
+
+    @Then("^I should see the user form page$")
+    public void iShouldSeeTheUserFormPage() throws Throwable {
+
+    }
+}
