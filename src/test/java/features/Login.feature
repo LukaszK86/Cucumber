f7cc8eb50061c4353878ed499685fe090cdf9bb4
Lukasz <lucas.kaczmarczyk@gmail.com>
2017-11-24 10:04:05 +0100

    LoginStep with dataTable values

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -3,6 +3,8 @@ Feature: LoginFeature
 
   Scenario:  Login with correct username and password
     Given I navigate to the login page
-    And I enter the username as "admin" and password as "adminpassword"
+    And I enter the following for Login
+      | UserName | Password      |
+      | admin    | adminpassword |
     And I click login button
     Then I should see the user form page
--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -1,9 +1,12 @@
 package steps;
 
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+
+import java.util.List;
 
 public class LoginStep {
     @Given("^I navigate to the login page$")
@@ -21,8 +24,12 @@ public class LoginStep {
         System.out.println("I should see the user form page\n");
     }
 
-    @And("^I enter the username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
-    public void iEnterTheUsernameAsAndPasswordAs(String username, String password) throws Throwable {
-        System.out.println("UserName is " + username + " and password is " + password + "\n");
+    @And("^I enter the following for Login$")
+    public void iEnterTheFollowingForLogin(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        System.out.println(data.get(0).get(0).toString());
+        System.out.println(data.get(0).get(1).toString());
+
+
     }
 }
